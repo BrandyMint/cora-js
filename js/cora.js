6ac2d07d53db81f7432d88f22a343a7625ea49ed
Evgeny Nikitin <yenyasinn@gmail.com>
2013-11-02 21:05:05 +0300

    Cora + demo

---
+++ js/cora.js
@@ -0,0 +1,59 @@
+/**
+ * Cora.js library.
+ */
+(function ($) {
+  $.fn.cora = function(options) {
+    var defaults = {
+       frequency: 3000
+    };
+
+    var options = $.extend(defaults, options);
+
+    var CoraElement = function(el) {
+      this.elem = el;
+      this.key = $(el).data('cora-key');
+      this.url = $(el).data('cora-url');
+      this.timer = null;
+
+      this.initTimer = function() {
+        if (!this.timer) {
+          this.timer = window.setTimeout($.proxy(function() {
+            this.save()}, this), options.frequency
+          );
+        }
+      }
+
+      $(this.elem).keyup({el: this}, function(e) {
+        e.data.el.initTimer();
+      });
+
+      this.save = function() {
+        this.timer = null;
+        this.send();
+      }
+
+      this.send = function() {
+        $.ajax({
+          url: this.url,
+          type: 'get',
+          data: {key: this.key, content: $(this.elem).html().trim()}
+        }).success(function(){
+
+        });
+      }
+    }
+
+    $(this).each(function() {
+      var obj = new CoraElement($(this));
+    });
+
+
+
+    return this;
+  }
+
+  $(document).ready(function(){
+    $('[role="cora-content"]').cora();
+  });
+
+})(jQuery);
